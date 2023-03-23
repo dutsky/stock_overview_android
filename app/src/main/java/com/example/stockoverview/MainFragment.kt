fdb8c677a5c1169984fc2af8a71005a9d7828f18
Denis Revin <denis.revin@gmail.com>
2023-03-23 04:12:14 +0400

    remove unused fab and `SecondFragment`

--- app/src/main/java/com/example/stockoverview/MainFragment.kt
+++ app/src/main/java/com/example/stockoverview/MainFragment.kt
@@ -5,15 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
-import com.example.stockoverview.databinding.FragmentSecondBinding
+import com.example.stockoverview.databinding.FragmentMainBinding
+import io.flutter.embedding.android.FlutterActivity
 
 /**
- * A simple [Fragment] subclass as the second destination in the navigation.
+ * A simple [Fragment] subclass as the default destination in the navigation.
  */
-class SecondFragment : Fragment() {
+class MainFragment : Fragment() {
 
-    private var _binding: FragmentSecondBinding? = null
+    private var _binding: FragmentMainBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -24,7 +24,7 @@ class SecondFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
 
-        _binding = FragmentSecondBinding.inflate(inflater, container, false)
+        _binding = FragmentMainBinding.inflate(inflater, container, false)
         return binding.root
 
     }
@@ -32,8 +32,10 @@ class SecondFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonSecond.setOnClickListener {
-            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
+        binding.buttonFirst.setOnClickListener {
+            startActivity(
+                FlutterActivity.createDefaultIntent(binding.root.context)
+            )
         }
     }
 
